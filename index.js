6a43955149db046b19c3c4a673494c8950f61f69
Sahana682001 <sahana.digiworq6@gmail.com>
2025-07-31 16:24:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import analyzeBrand from './analyze-brand.js';
 
+
 dotenv.config();
 const app = express();
 
@@ -13,7 +14,11 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+// Existing routes
 app.use('/api', analyzeBrand);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
